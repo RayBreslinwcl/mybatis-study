00b0f14f0bab7fdfd1d7d21fbdc1f78479ba317c
Raywcl <2728928100@qq.com>
2020-04-06 07:29:18 +0800

    多对一 1.初始化环境测试

--- mybatis-07/src/main/java/com/rui/dao/TeacherMapper.java
+++ mybatis-07/src/main/java/com/rui/dao/TeacherMapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface TeacherMapper {
     //获取老师
-//    List<Teacher> getTeacher();
+    List<Teacher> getTeacher0();
 
     //获取指定老师下的所有学生及老师的信息
     Teacher getTeacher(@Param("tid") int id);
